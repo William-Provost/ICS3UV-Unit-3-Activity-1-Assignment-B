389855811a81c759972a31e06b09558309e95502
default <william.provost@stu.ocsb.ca>
2025-11-05 23:32:26 +0000

    calculated tax, tip and total cost of meal in TS and Go

---
+++ Unit3D.go
@@ -0,0 +1,39 @@
+/**
+ * @author William Provost
+ * @version 1.0.0
+ * @date 2025-11-05
+ * @fileoverview This program calculates the total cost of a meal at a restaurant,
+ * including tax and tip, and divides it equally among the number of people splitting the bill.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	// initialize constants
+	const HST_RATE float64 = 0.13
+	const TIP_RATE float64 = 0.15
+
+	// INPUT
+	// user-provided values
+	const mealCost float64 = 315.99
+	const numPeople float64 = 5
+
+	// PROCESS
+	// calculate tax, tip, total, and cost per person
+	taxAmount := mealCost * HST_RATE
+	tipAmount := mealCost * TIP_RATE
+	totalCost := mealCost + taxAmount + tipAmount
+	costPerPerson := totalCost / numPeople
+
+	// OUTPUT
+	// display all calculated results
+	fmt.Println("The subtotal (cost before tax and tip) is: $", mealCost)
+	fmt.Println("The tax amount (13%) is: $", taxAmount)
+	fmt.Println("The tip amount (15%) is: $", tipAmount)
+	fmt.Println("The total cost (including tax and tip) is: $", totalCost)
+	fmt.Println("The total cost per person (", numPeople, " people) is: $", costPerPerson)
+
+	fmt.Println("Done.")
+}
---
+++ Unit3D.ts
@@ -0,0 +1,33 @@
+/**
+ * @author William Provost
+ * @version 1.0.0
+ * @date 2025-11-05
+ * @fileoverview This program calculates the total cost of a meal at a restaurant,
+ * including tax and tip, and divides it equally among the number of people splitting the bill.
+*/
+
+// initialize constants
+const HST_RATE: number = 0.13;
+const TIP_RATE: number = 0.15;
+
+// INPUT
+// user-provided numbers
+const mealCost: number = 315.99;
+const numPeople: number = 5;
+
+// PROCESS
+// calculate tax, tip, total, and cost per person
+let taxAmount: number = mealCost * HST_RATE;
+let tipAmount: number = mealCost * TIP_RATE;
+let totalCost: number = mealCost + taxAmount + tipAmount;
+let costPerPerson: number = totalCost / numPeople;
+
+// OUTPUT
+// display all calculated results
+console.log("The subtotal is: $" + mealCost);
+console.log("The tax amount (13%) is: $" + taxAmount);
+console.log("The tip amount (15%) is: $" + tipAmount);
+console.log("The total cost (including tax and tip) is: $" + totalCost);
+console.log("The total cost per person (" + numPeople + " people) is: $" + costPerPerson);
+
+console.log("Done.");
