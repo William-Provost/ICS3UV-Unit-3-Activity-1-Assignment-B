8f156df9b9ee2995212940b23c2af20ffafd70d3
default <william.provost@stu.ocsb.ca>
2025-11-17 20:03:50 +0000

    fixed lint issues

--- Unit3D.ts
+++ Unit3D.ts
@@ -17,17 +17,17 @@ const numPeople: number = 5;
 
 // PROCESS
 // calculate tax, tip, total, and cost per person
-const taxAmount: number = mealCost * HST_RATE;
+const taxAmount1: number = mealCost * HST_RATE;
 const tipAmount: number = mealCost * TIP_RATE;
-const totalCost: number = mealCost + taxAmount + tipAmount;
-const costPerPerson: number = totalCost / numPeople;
+const totalCost1: number = mealCost + taxAmount1 + tipAmount;
+const costPerPerson: number = totalCost1 / numPeople;
 
 // OUTPUT
 // display all calculated results
 console.log("The subtotal is: $" + mealCost);
-console.log("The tax amount (13%) is: $" + taxAmount);
+console.log("The tax amount (13%) is: $" + taxAmount1);
 console.log("The tip amount (15%) is: $" + tipAmount);
-console.log("The total cost (including tax and tip) is: $" + totalCost);
+console.log("The total cost (including tax and tip) is: $" + totalCost1);
 console.log("The total cost per person (" + numPeople + " people) is: $" + costPerPerson);
 
 console.log("\nDone.");
